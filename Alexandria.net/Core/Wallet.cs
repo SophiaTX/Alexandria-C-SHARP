d992433c103ffd8f08ec80b63f2c4df9a4bfe9c7
sanjivjha <sanjiv.jha@sophiatx.com>
2018-06-01 16:57:01 +0200

    * Wallet.cs: Dllimport functions added
    * libalexandria.dylib:
    * project.assets.json:
    * Alexandria.net.csproj.nuget.cache:
    * Alexandria.net.csproj.nuget.g.props:
    * Alexandria.net.csprojResolveAssemblyReference.cache:

--- Alexandria.net/Core/Wallet.cs
+++ Alexandria.net/Core/Wallet.cs
@@ -1,7 +1,10 @@
-﻿namespace Alexandria.net.Core
+﻿using System.Runtime.InteropServices;
+
+namespace Alexandria.net.Core
 {
 	public class Wallet : Alexandria.net.API.Wallet
 	{
+		private const string libAddress = "libalexandria.dylib";
 		#region Constructors
 
 		public Wallet(string hostname = "127.0.0.1", ushort port = 8091) : base(hostname, port)
@@ -9,5 +12,13 @@
 		}
 
 		#endregion
+		[DllImport(libAddress)]
+        public static extern bool generate_private_key(byte[] private_key);
+        [DllImport(libAddress)]
+        public static extern bool get_transaction_digest(string transaction, byte[] digest);
+        [DllImport(libAddress)]
+        public static extern bool sign_digest(string digest, string private_key, byte[] signed_digest);
+        [DllImport(libAddress)]
+        public static extern bool add_signature(string transaction, string signature, byte[] signed_tx);
     }
 }
