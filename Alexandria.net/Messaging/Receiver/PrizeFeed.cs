b2d8831682bc87c782678086b044f258d14f3f53
sanjivjha <sanjiv.jha@sophiatx.com>
2018-07-25 13:07:48 +0200

    functions updated

---
+++ Alexandria.net/Messaging/Receiver/PrizeFeed.cs
@@ -0,0 +1,16 @@
+﻿namespace Alexandria.net.Messaging.Receiver
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PrizeFeed
+    {/// <summary>
+     /// 
+     /// </summary>
+        public string Value{ get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public PrizeFeedQuote Feed { get; set; }
+    }
+}
