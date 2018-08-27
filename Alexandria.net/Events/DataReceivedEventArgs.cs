f7005bb846122d94718aa52aa56b59a60965efd9
martynharler <martyn_equidato@outlook.com>
2018-08-27 12:55:08 +0200

    Events folder and obects included in the project

---
+++ Alexandria.net/Events/DataReceivedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Alexandria.net.Messaging.Responses;
+
+namespace Alexandria.net.Events
+{
+    public class DataReceivedEventArgs : EventArgs
+    {
+        public DataReceivedEventArgs(ReceivedDocumentResponse message)
+        {
+            Message = message;
+        }
+
+        public ReceivedDocumentResponse Message { get; set; }
+    }
+}
---
+++ Alexandria.net/Events/DataReceivedEventHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Alexandria.net.Events
+{
+    /// <summary>
+    /// </summary>
+    public delegate void DataReceivedEventHandler(object sender, DataReceivedEventArgs e);
+}
