de651aa0a98f96ab5dc5680a7925507a0f1f61e4
martynharler <martyn_equidato@outlook.com>
2018-08-30 11:16:25 +0200

    fixes to unittest references

--- Test/UnitTest/WalletTests.cs
+++ Test/UnitTest/WalletTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading;
+using Alexandria.net.Enums;
+using Alexandria.net.Events;
+using Alexandria.net.Messaging.Receiver;
 using Xunit;
 
 namespace UnitTest
